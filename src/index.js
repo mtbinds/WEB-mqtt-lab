27946a06cc70f82e029b792215d11dd6a78d6720
Yoann Pigné <yoann.pigne@univ-lehavre.fr>
2018-10-08 23:33:22 +0200

    Root project with WebSocket and poxie for the mqtt server

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// On pourrait utiliser un WebSocket pour se connecter au serveur MQTT
+// Mais on préférera utiliser un bibliothèque spécifique au protocole. 
+// 
+// Ne pas utiliser directement une WebSocket...
+const ws = new WebSocket("ws://" + window.location.host+ "/socket");
+ws.onopen = (event) => {
+  console.log("We are connected.");
+};

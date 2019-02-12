c614789b0843be1daf0fa79e2983ef4bd3ea2390
Yoann Pigné <yoann.pigne@univ-lehavre.fr>
2019-02-12 18:12:06 +0100

    update mqtt server

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import './index.css';
 
+const url  = "ws://" + window.location.host+ "/socket";
+
+
 // On pourrait utiliser un WebSocket pour se connecter au serveur MQTT
 // Mais on préférera utiliser un bibliothèque spécifique au protocole. 
 // 
 // Ne pas utiliser directement une WebSocket...
-const ws = new WebSocket("ws://" + window.location.host+ "/socket");
+const ws = new WebSocket(url);
+ws.onclose = (event) => console.log("Direct connection closed. Cool!")
 ws.onopen = (event) => {
-  console.log("We are connected.");
+  console.log("We are connected. But let's NOT use this connection because we want to use a MQTT library to handle the WebSocket.");
+  ws.close()
 };
+

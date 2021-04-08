331663c57db9e060eea0cc6d72d472680c0281c6
Madjid Taoualit <madjid.taoualit@etu.univ-lehavre.fr>
2021-04-08 16:15:10 +0200

    Updating Project => Adding necessary code and scripts

--- src/index.js
+++ src/index.js
@@ -1,16 +1,64 @@
 import './index.css';
+import './sensor';
+import {Sensor} from "./sensor";
 
-const url  = "ws://random.pigne.org:9001";
+const mqtt = require('mqtt')
+const client  = mqtt.connect('mqtt://hyperespaceb1.univ-lehavre.fr:8080');
+
+const sensors = [];
+
+const ul = document.getElementById('messages');
+
+client.on('connect', function () {
+  console.log('oui');
+  client.subscribe('value/+');
+});
+
+client.on('message', function (topic, message) {
+  let id = topic.split('/')[1];
+  let jsonMessage = JSON.parse(message.toString());
+  let sensorExist = false;
 
 
-// On pourrait utiliser un WebSocket pour se connecter au serveur MQTT
-// Mais on préférera utiliser un bibliothèque spécifique au protocole. 
-// 
-// Ne pas utiliser directement une WebSocket...
-const ws = new WebSocket(url);
-ws.onclose = (event) => console.log("Direct connection closed. Cool!")
-ws.onopen = (event) => {
-  console.log("We are connected. But let's NOT use this connection because we want to use a MQTT library to handle the WebSocket.");
-  ws.close()
-};
+  for(let s of sensors){
+    if(s.id === id){
+      s.update(jsonMessage.value);
+      let show = s.toString().split('\n');
+      console.log(show);
+      document.getElementById('nom' + s.id).textContent = show[0];
+      document.getElementById('rel' + s.id).textContent = show[1];
+      document.getElementById('avg' + s.id).textContent = show[2];
+      sensorExist = true;
+    }
+  }
+
+  if(sensorExist === false){
+    let newSensor = createSensor(id, jsonMessage)
+    sensors.push(newSensor);
+    let li = document.createElement('li');
+    let nom = document.createElement('p');
+    let rel = document.createElement('p');
+    let avg = document.createElement('p');
+    li.id = newSensor.id;
+    nom.id = 'nom' + newSensor.id;
+    rel.id = 'rel' + newSensor.id;
+    avg.id = 'avg' + newSensor.id;
+    li.append(nom, rel, avg);
+    ul.appendChild(li);
+  }
+
+});
+
+function createSensor(id, data){
+  let sensorData = [{
+    "id": id,
+    "name": data.name,
+    "type": data.type,
+    "value": data.value
+  }];
+  return Sensor.createSensors(sensorData);
+}
+
+
+
 
